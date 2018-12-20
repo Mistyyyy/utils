8a6bdba7bb7ae2c31795ff34422411ebdac6f0d8
dingchunlei <dingchunlei@shein.com>
2018-12-20 10:59:25 +0800

    recursion fib

--- Vue_resource/code_split/catched.js
+++ Vue_resource/code_split/catched.js
@@ -9,3 +9,15 @@ const camelize = cache(str => {
   const reg = /-(\w)/
   return str.replace(reg, (_,c) => c.toUpperCase())
 })
+
+const fib = cache(n => {
+  const _fib = (result, n) => {
+    const copyResult = [...result].reverse()
+    const [first, second] = copyResult
+    return n < 3
+      ? [...result]
+      : _fib([...result,first + second], --n)
+  }
+  return _fib([0,1], n)
+})
+
